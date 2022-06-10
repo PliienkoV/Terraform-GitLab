4cae034d467d8ee036c833ea68c664a2fc099cc0
Volodymyr Pliienko <volodymyr.pliienko@gmail.com>
2022-06-10 14:14:51 +0300

    Added variables Terraform-GitLab

--- variables.tf
+++ variables.tf
@@ -1,5 +1,12 @@
+# Provider vars
+variable "gitlab_url" {
+  description = "GitLab server URL"
+  type = string
+  default = "https://192.168.0.199/api/v4/"
+}
+
 variable "gitlab_token" {
-    description = "GitLab Token"
-    type = string
-        
+  description = "GitLab Token"
+  type = string
+  default = "5bZuyii9_2FTqeZRJbUB"
 }
